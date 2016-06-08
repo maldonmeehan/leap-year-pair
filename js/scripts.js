54c4ac263522471b213839103f195bd777098160
Marty Fitchen <mfitchen@gmail.com>
2016-06-07 22:58:01 -0700

    add initial directories and files for HTML css and js plus a README

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var leapYear = function(year) {
+  return false;
+};
+
+// jQuery User Interface
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
